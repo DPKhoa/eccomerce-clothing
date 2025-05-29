e28213d66390cc21fc55df4718e803668c47141f
Nguyễn Anh Khoa <anhk312001@gmail.com>
2025-05-29 14:16:39 +0700

    first-commit

---
+++ src/main/java/com/app/projectstyleecommerce/ProjectStyleEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.app.projectstyleecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStyleEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectStyleEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/projectstyleecommerce/ProjectStyleEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.projectstyleecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectStyleEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
